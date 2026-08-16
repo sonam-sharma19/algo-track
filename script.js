7925d1a51e9aaab22511d7516d9e3c6fc845bce3
Sonam Sharma <sharmasonam9847@gmail.com>
2026-08-16 19:39:12 +0530

    Add topics completed counter

--- script.js
+++ script.js
@@ -13,3 +13,15 @@ solveButton.addEventListener("click", function () {
     problemsSolved++;
     problemCount.textContent = problemsSolved;
 });
+
+// Topics Completed Counter
+
+let topicsCompleted = 0;
+
+const topicCount = document.getElementById("topics-completed");
+const topicButton = document.getElementById("topic-btn");
+
+topicButton.addEventListener("click", function () {
+    topicsCompleted++;
+    topicCount.textContent = topicsCompleted;
+});
